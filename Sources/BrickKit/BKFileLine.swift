8a747ac9389b49e9eab8664394ef3cb485811f13
iain <iain@falsevictories.com>
2025-02-26 22:41:20 +0000

    Correct the transform matrix

--- Sources/BrickKit/BKFileLine.swift
+++ Sources/BrickKit/BKFileLine.swift
@@ -73,9 +73,9 @@ struct BKSubpart {
         let t2 = SCNVector3(xStr: components[8], yStr: components[9], zStr: components[10]) ?? SCNVector3Zero
         let t3 = SCNVector3(xStr: components[11], yStr: components[12], zStr: components[13]) ?? SCNVector3Zero
         
-        transform = SCNMatrix4(m11: t1.x, m12: t1.y, m13: t1.z, m14: 0,
-                               m21: t2.x, m22: t2.y, m23: t2.z, m24: 0,
-                               m31: t3.x, m32: t3.y, m33: t3.z, m34: 0,
+        transform = SCNMatrix4(m11: t1.x, m12: t2.x, m13: t3.x, m14: 0,
+                               m21: t1.y, m22: t2.y, m23: t3.y, m24: 0,
+                               m31: t1.z, m32: t2.z, m33: t3.z, m34: 0,
                                m41: position.x, m42: position.y, m43: position.z, m44: 1)
         filename = components[14].replacingOccurrences(of: "\\", with: "/")
     }
