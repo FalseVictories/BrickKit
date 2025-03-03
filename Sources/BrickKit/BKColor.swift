b860864d5193cbe3bdc9e19fd10d4c03c568bfc8
iain <iain@falsevictories.com>
2025-03-03 22:59:22 +0000

    Handle colours
    Create a colour manager and colour class

---
+++ Sources/BrickKit/BKColor.swift
@@ -0,0 +1,146 @@
+//
+//  File.swift
+//  BrickKit
+//
+//  Created by iain on 28/02/2025.
+//
+
+import AppKit
+import Foundation
+
+public typealias BKColorCode = UInt16
+public enum BKFinish {
+    case chrome
+    case pearlescent
+    case rubber
+    case matte
+    case metallic
+    case material
+}
+
+public struct BKColor {
+    public let name: String
+    public var code: BKColorCode = 0
+    public var color: NSColor = .black
+    public var edgeColor: NSColor?
+    public var edgeCode: BKColorCode?
+    public var alpha: UInt8 = 255
+    public var luminosity: UInt8 = 0
+    
+    public var finish: BKFinish?
+    
+    public init?(from line: String) {
+        let trimmedLine = line.trimmingCharacters(in: .whitespacesAndNewlines)
+        let components = trimmedLine.split(separator: " ")
+        
+        if components.count < 9 {
+            return nil
+        }
+        
+        if components[0] != "0" || components[1] != "!COLOUR" {
+            return nil
+        }
+        
+        name = String(components[2])
+        
+        var index = 3
+        while index < components.count {
+            switch components[index] {
+            case "CODE":
+                code = BKColorCode(String(components[index + 1])) ?? 0
+                index += 2
+                break
+                
+            case "VALUE":
+                let hex = String(components[index + 1])
+                color = .init(fromHex: hex) ?? .gray
+                index += 2
+                break
+                
+            case "EDGE":
+                edgeCode = BKColorCode(String(components[index + 1]))
+                if edgeCode == nil {
+                    let hex = String(components[index + 1])
+                    edgeColor = .init(fromHex: hex)
+                }
+                index += 2
+                break
+                
+            case "ALPHA":
+                alpha = UInt8(String(components[index + 1])) ?? 255
+                color = color.withAlphaComponent(CGFloat(alpha) / 255.0)
+                index += 2
+                break
+                
+            case "LUMINANCE":
+                luminosity = UInt8(String(components[index + 1])) ?? 0
+                index += 2
+                break
+                
+            case "CHROME":
+                finish = .chrome
+                index = .max
+                break
+                
+            case "PEARLESCENT":
+                finish = .pearlescent
+                index = .max
+                break
+                
+            case "RUBBER":
+                finish = .rubber
+                index = .max
+                break
+                
+            case "MATTE_METALLIC":
+                finish = .matte
+                index = .max
+                break
+                
+            case "METAL":
+                finish = .metallic
+                index = .max
+                break
+                
+            case "MATERIAL":
+                finish = .material
+                index = .max
+                break
+                
+            default:
+                index += 1
+                break
+            }
+        }
+    }
+}
+
+fileprivate extension NSColor {
+    convenience init?(fromHex hexColor: String, alpha: CGFloat = 1.0) {
+        var result : NSColor? = nil
+        var colorCode : UInt32 = 0
+
+        var hex: String = hexColor
+        if hexColor.hasPrefix("#") {
+            hex = String(hexColor[hexColor.index(hexColor.startIndex, offsetBy: 1)...])
+        } else {
+            hex = hexColor
+        }
+
+        let scanner = Scanner(string: hex)
+        let success = scanner.scanHexInt32(&colorCode)
+
+        if !success {
+            return nil
+        }
+
+        let redByte = (colorCode >> 16) & 0xFF
+        let greenByte = (colorCode >> 8) & 0xFF
+        let blueByte = colorCode & 0xFF
+        
+        self.init(calibratedRed: CGFloat(redByte) / 256.0,
+                  green: CGFloat(greenByte) / 256.0,
+                  blue: CGFloat(blueByte) / 256.0,
+                  alpha: alpha)
+    }
+}
---
+++ Sources/BrickKit/BKColorManager.swift
@@ -0,0 +1,48 @@
+//
+//  File.swift
+//  BrickKit
+//
+//  Created by iain on 01/03/2025.
+//
+
+import AppKit
+import Foundation
+
+@MainActor
+public class BKColorManager {
+    private var colors: [UInt16: BKColor]
+    
+    private static var _shared: BKColorManager?
+    public static var shared: BKColorManager {
+        guard let shared = _shared else {
+            fatalError("BKColorManager not initialized")
+        }
+        
+        return shared
+    }
+    
+    public static func createSharedColorManager(from file: String) async throws {
+        guard _shared == nil else {
+            return
+        }
+        
+        _shared = try await BKColorManager(from: file)
+    }
+    
+    private init(from filePath: String) async throws {
+        colors = [:]
+        
+        let url = URL(fileURLWithPath: filePath)
+        for try await line in url.lines {
+            if let color = BKColor(from: line) {
+                colors[color.code] = color
+            }
+        }
+    }
+}
+
+extension BKColorManager {
+    public func colorForID(_ id: UInt16) -> NSColor? {
+        return colors[id]?.color ?? nil
+    }
+}
